599ffcc51a7dcfb45d3fb9e7fbfd6a702eb111dd
Efrain Pinto <efrapp@gmail.com>
2020-11-06 18:32:08 -0500

    Setup Devise
    - Set custom controllers and views for sign-up and sign-in actions
    - Set custom redirections for sign-up and sign-in actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  get 'users/home'
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
+
+  devise_scope :user do
+    root to: 'users/registrations#new'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
