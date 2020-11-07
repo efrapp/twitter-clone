182af80eea9f8423445e46ab8c0aadef004e821d
Efrain Pinto <efrapp@gmail.com>
2020-11-07 11:49:42 -0500

    Add followers and followings counters in the home page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!, except: :show
 
   def show
+    @followers = current_user.followers
+    @followings = current_user.followings
   end
 end
